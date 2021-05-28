3bc94f209fc67a8197c32fa543797ed507739411
caturprasatya <caturprasatya1224@gmail.com>
2021-05-28 19:59:53 +0700

    init rect native

---
+++ data.js
@@ -0,0 +1,85 @@
+const financial = [
+  {
+    status: 'income',
+    title: '',
+    amount: ''
+  },
+  {
+    status: '',
+    title: '',
+    amount: ''
+  },
+  {
+    status: 'income',
+    title: '',
+    amount: ''
+  },
+  {
+    status: '',
+    title: '',
+    amount: ''
+  },
+  {
+    status: 'income',
+    title: '',
+    amount: ''
+  },
+  {
+    status: '',
+    title: '',
+    amount: ''
+  },
+  {
+    status: 'income',
+    title: '',
+    amount: ''
+  },
+  {
+    status: '',
+    title: '',
+    amount: ''
+  },
+]
+
+const financial = [
+  {
+    status: 'checkIn',
+    title: '',
+    quantity: ''
+  },
+  {
+    status: 'checkOut',
+    title: '',
+    quantity: ''
+  },
+  {
+    status: 'checkIn',
+    title: '',
+    quantity: ''
+  },
+  {
+    status: 'checkOut',
+    title: '',
+    quantity: ''
+  },
+  {
+    status: 'checkIn',
+    title: '',
+    quantity: ''
+  },
+  {
+    status: 'checkOut',
+    title: '',
+    quantity: ''
+  },
+  {
+    status: 'checkIn',
+    title: '',
+    quantity: ''
+  },
+  {
+    status: 'checkOut',
+    title: '',
+    quantity: ''
+  },
+]
